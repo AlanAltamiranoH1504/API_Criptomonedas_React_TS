b38e7d1e355f901396f3835f25c59c1abb53000a
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-07-25 09:17:18 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <>
+            <h1 className="text-center bg-orange-500">Cotizador Criptos</h1>
+        </>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,tsx,jsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                fjalla: ['"Fjalla One"', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+}
+
